ffa6ef24b79bdfa3324ce02d632e29181fc9782f
alex <alex@pdp7.net>
2023-11-10 17:39:56 +0100

    Remove processed file

--- examples/host_example.py
+++ examples/host_example.py
@@ -8,7 +8,8 @@ if __name__ == "__main__":
     metric_exporter = otel.create_grpc_metric_exporter(sys.argv[1], True)
     batcher = otel.MetricBatcher(metric_exporter)
 
-    for line in pathlib.Path(sys.argv[2]).read_text().splitlines():
+    file = pathlib.Path(sys.argv[2])
+    for line in file.read_text().splitlines():
         print(line)
         try:
             perf_data = perfdata.parse_host_perfdata(line)
@@ -22,3 +23,4 @@ if __name__ == "__main__":
                 batcher.record_gauge(perf_data.timestamp, perf_data.host, "system.network.ping.warn", int(float(metric.warn) * 1000), "ns")
                 batcher.record_gauge(perf_data.timestamp, perf_data.host, "system.network.ping.crit", int(float(metric.crit) * 1000), "ns")
     batcher.batch()
+    file.unlink()
